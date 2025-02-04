8ee0f58b629c7449b4d2750a47ab94a155ad2fd5
2mauapp <130876816+2mauapp@users.noreply.github.com>
2025-02-04 15:05:30 +0700

    Update oauth.go

--- auth/oauth.go
+++ auth/oauth.go
@@ -87,7 +87,8 @@ func getConfig(clientId, clientSecret string) *oauth2.Config {
 		ClientID:     clientId,
 		ClientSecret: clientSecret,
 		Scopes:       []string{"https://www.googleapis.com/auth/drive"},
-		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
+		// RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
+		RedirectURL:  "http://localhost:8080",
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
 			TokenURL: "https://accounts.google.com/o/oauth2/token",
